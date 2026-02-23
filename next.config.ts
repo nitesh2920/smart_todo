ddfee0f6a125fba3e9d9981aaef80b49e5183548
nitesh2920 <niteshtiwari0234@gmail.com>
2026-02-23 20:31:42 +0530

    chore: tailiwndcss bug resolved

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Set turbopack root to the current project directory to avoid resolving upwards
+  // to C:\Users\nites\package-lock.json
+ 
 };
 
 export default nextConfig;
