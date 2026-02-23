300710d212fcbffa5af77d430cd2de7d4a8b3231
nitesh2920 <niteshtiwari0234@gmail.com>
2026-02-23 20:32:28 +0530

    feat: adding tanstack dev tools

--- components/providers.tsx
+++ components/providers.tsx
@@ -3,6 +3,8 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useState } from "react";
 
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
 export default function Providers({ children }: { children: React.ReactNode }) {
     const [queryClient] = useState(
         () =>
@@ -18,6 +20,7 @@ export default function Providers({ children }: { children: React.ReactNode }) {
     return (
         <QueryClientProvider client={queryClient}>
             {children}
+            <ReactQueryDevtools initialIsOpen={false} />
         </QueryClientProvider>
     );
 }
